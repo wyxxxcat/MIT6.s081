2fe03cb3ed9ffee299824039b0205e272d612f61
wyxxxcat <1520358997@qq.com>
2024-01-09 11:11:04 +0800

    update; git commit -m update

---
+++ user/pingpong.c
@@ -0,0 +1,29 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main(int argc, char **argv)
+{
+    int ppSon[2];
+    int ppFa[2];
+
+    // pipe 0 read 1 write
+    pipe(ppSon); // Fa -> Son
+    pipe(ppFa);  // Son -> Fa
+    if (fork() != 0)
+    { // Fa
+        write(ppSon[1], "1", 1);
+        char buf;
+        read(ppFa[0], &buf, 1);
+        printf("%d: received pong\n", getpid());
+        wait(0);
+    }
+    else
+    { // Son
+        char buf;
+        read(ppSon[0], &buf, 1); // 3. Fa->Son
+        printf("%d: received ping\n", getpid());
+        write(ppFa[1], &buf, 1); // Son->Fa
+    }
+    exit(0);
+}
