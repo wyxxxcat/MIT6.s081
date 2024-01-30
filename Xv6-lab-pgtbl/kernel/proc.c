715b233d7f98017785e6934b13865e81bbd03739
wyxxxcat <1520358997@qq.com>
2024-01-30 14:28:29 +0800

    speed up syscall

--- Xv6-lab-pgtbl/kernel/proc.c
+++ Xv6-lab-pgtbl/kernel/proc.c
@@ -111,13 +111,19 @@ allocproc(void)
 
 found:
   p->pid = allocpid();
-
   // Allocate a trapframe page.
   if ((p->trapframe = (struct trapframe *)kalloc()) == 0)
   {
     release(&p->lock);
     return 0;
   }
+
+  if ((p->usyscall = (struct usyscall *)kalloc()) == 0)
+  {
+    release(&p->lock);
+    return 0;
+  }
+  p->usyscall->pid = p->pid;
 
   // An empty user page table.
   p->pagetable = proc_pagetable(p);
@@ -157,8 +163,13 @@ freeproc(struct proc *p)
   if (p->trapframe)
     kfree((void *)p->trapframe);
   p->trapframe = 0;
+
+  if (p->usyscall)
+    kfree(p->usyscall);
+  p->usyscall = 0;
   if (p->pagetable)
     proc_freepagetable(p->pagetable, p->sz);
+  
   p->pagetable = 0;
   p->sz = 0;
   p->pid = 0;
@@ -210,6 +221,15 @@ proc_pagetable(struct proc *p)
     uvmfree(pagetable, 0);
     return 0;
   }
+  //map the usyscall
+  if (mappages(pagetable, USYSCALL, PGSIZE,
+               (uint64)(p->usyscall), PTE_R | PTE_U) < 0)
+  {
+    uvmunmap(pagetable, USYSCALL, 1, 0);
+    uvmunmap(pagetable, TRAMPOLINE, 1, 0);
+    uvmfree(pagetable, 0);
+    return 0;
+  }
 
   return pagetable;
 }
@@ -218,6 +238,7 @@ proc_pagetable(struct proc *p)
 // physical memory it refers to.
 void proc_freepagetable(pagetable_t pagetable, uint64 sz)
 {
+  uvmunmap(pagetable,USYSCALL,1,0);
   uvmunmap(pagetable, TRAMPOLINE, 1, 0);
   uvmunmap(pagetable, TRAPFRAME, 1, 0);
   uvmfree(pagetable, sz);
--- Xv6-lab-pgtbl/user/ulib.c
+++ Xv6-lab-pgtbl/user/ulib.c
@@ -134,3 +134,4 @@ memcpy(void *dst, const void *src, uint n)
 {
   return memmove(dst, src, n);
 }
+
